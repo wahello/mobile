81a21285da9ddd963dd985186365588dfe9d41c1
Carlo Conflitti <carlo.conflitti@gmail.com>
2019-11-03 13:27:41 +0100

    :art: aggiunti model per chiamate inserimento incontro

---
+++ lib/blocs/stuff/calls_model.dart
@@ -0,0 +1,92 @@
+class Gender {
+  int id;
+  String name;
+
+  Gender(this.id, this.name);
+
+  Gender.fromJson(Map<String, dynamic> json) {
+    this.id = json['ID'];
+    this.name = json['Name'];
+  }
+
+  Map<String, dynamic> toJson() => {'ID': id, 'Name': name};
+}
+
+class Championship {
+  int id;
+  String name;
+  String createdAt;
+  String updatedAt;
+  String deletedAt;
+
+  Championship(
+      this.id, this.name, this.createdAt, this.deletedAt, this.updatedAt);
+
+  Championship.fromJson(Map<String, dynamic> json) {
+    this.id = json['ID'];
+    this.name = json['Name'];
+    this.createdAt = json['CreatedAt'];
+    this.deletedAt = json['DeletedAt'];
+    this.updatedAt = json['UpdatedAt'];
+  }
+
+  Map<String, dynamic> toJson() => {
+        'ID': id,
+        'Name': name,
+        'CreatedAt': createdAt,
+        'DeletedAt': deletedAt,
+        'UpdatedAt': updatedAt
+      };
+}
+
+class Match {
+  int id;
+  String name;
+  String createdAt;
+  String updatedAt;
+  String deletedAt;
+
+  Match(this.id, this.name, this.createdAt, this.deletedAt, this.updatedAt);
+
+  Match.fromJson(Map<String, dynamic> json) {
+    this.id = json['ID'];
+    this.name = json['Name'];
+    this.createdAt = json['CreatedAt'];
+    this.deletedAt = json['DeletedAt'];
+    this.updatedAt = json['UpdatedAt'];
+  }
+
+  Map<String, dynamic> toJson() => {
+        'ID': id,
+        'Name': name,
+        'CreatedAt': createdAt,
+        'DeletedAt': deletedAt,
+        'UpdatedAt': updatedAt
+      };
+}
+
+class Category {
+  int id;
+  String name;
+  String createdAt;
+  String updatedAt;
+  String deletedAt;
+
+  Category(this.id, this.name, this.createdAt, this.deletedAt, this.updatedAt);
+
+  Category.fromJson(Map<String, dynamic> json) {
+    this.id = json['ID'];
+    this.name = json['Name'];
+    this.createdAt = json['CreatedAt'];
+    this.deletedAt = json['DeletedAt'];
+    this.updatedAt = json['UpdatedAt'];
+  }
+
+  Map<String, dynamic> toJson() => {
+        'ID': id,
+        'Name': name,
+        'CreatedAt': createdAt,
+        'DeletedAt': deletedAt,
+        'UpdatedAt': updatedAt
+      };
+}
