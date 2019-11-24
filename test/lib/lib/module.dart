1fcd20538426b9dd4aee5a06b277c31fd9acbcf2
Alberto Cipollina <alberto.cipollina@studenti.polito.it>
2019-11-24 18:42:19 +0100

    nuova struttura per il campo

---
+++ test/lib/lib/module.dart
@@ -0,0 +1,8 @@
+class Module {
+  //ARRAY CHE CONTIENE LE POSIZIONI DEI GIOCATORI NELLA "SCACCHIERA".
+  final List<List<int>> fieldPostion;
+  //NOME DEL MODULO. ES:4-4-2
+  final String name;
+
+  Module({this.fieldPostion, this.name});
+}
