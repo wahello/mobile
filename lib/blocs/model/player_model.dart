97bd6c8b819cd311fa139acd416cbfd2d4e5654a
fabrizioddera <fabrizioddera@gmail.com>
2019-11-29 17:33:17 +0100

    nuove logiche per il campo

--- lib/blocs/model/player_model.dart
+++ lib/blocs/model/player_model.dart
@@ -7,6 +7,7 @@ class Player {
   final int id;
   final String name;
   String ruolo;
+  String numero;
   String posizione;
   Player({this.id, this.name, this.ruolo, this.posizione});
   factory Player.fromJson(Map<String, dynamic> json) => _$PlayerFromJson(json);
--- lib/blocs/stuff/module442.dart
+++ lib/blocs/stuff/module442.dart
@@ -10,6 +10,7 @@ import 'field.dart';
 class Module442 extends StatefulWidget {
   final double lato;
   final Field posizioni = Field();
+  final Map<int, Player> playersPlaced = new Map();
   final List<Player> players = new List(11);
 
   Module442({Key key, @required this.lato}) : super(key: key);
@@ -21,6 +22,7 @@ class Module442 extends StatefulWidget {
 
 class Module442State extends State<Module442> {
   var _tapPosition;
+  var _tapIndex;
 
 // Passo come parametro il giocatore selezionato
   void _showCustomMenu() {
@@ -28,7 +30,10 @@ class Module442State extends State<Module442> {
 
     showMenu(
             context: context,
-            items: <PopupMenuEntry<int>>[PlayerMenu()],
+            items: <PopupMenuEntry<int>>[
+              PlayerMenu(
+                  widget.playersPlaced, widget.players, _tapIndex)
+            ],
             position: RelativeRect.fromRect(
                 _tapPosition & Size(40, 40), // smaller rect, the touch area
                 Offset.zero & overlay.size // Bigger rect, the entire screen
@@ -39,6 +44,10 @@ class Module442State extends State<Module442> {
 
   void _storePosition(TapDownDetails details) {
     _tapPosition = details.globalPosition;
+  }
+
+  void _setIndex(int currentIndex) {
+    _tapIndex = currentIndex;
   }
 
   @override
@@ -64,79 +73,97 @@ class Module442State extends State<Module442> {
                               index == 79 || //=> posizioni[8][7]
                               index == 73 || //=> posizioni[8][1]
                               index == 112)
-                          ? state is FootballFieldInitiated
-                              ? GestureDetector(
-                                  // This does not give the tap position ...
-                                  onLongPress: _showCustomMenu,
-                                  onTapDown: _storePosition,
-                                  child: Container(
-                                    child: Icon(
-                                      Soccerplayer.soccer_player,
-                                      size: widget.lato,
-                                    ),
-                                  ))
-                              : state is FootballFieldCreated
-                                  ? GestureDetector(
-                                      // This does not give the tap position ...
-                                      onLongPress: _showCustomMenu,
-                                      onTapDown: _storePosition,
-                                      child: Container(
-                                        child: Icon(
-                                          Soccerplayer.soccer_player,
-                                          size: widget.lato,
-                                        ),
-                                      ))
-                                  : state is FootballFieldEdit
-                                      ? DragTarget(builder:
-                                          (BuildContext context,
-                                              List candidateData,
-                                              List rejectedData) {
-                                          return (index == 11 ||
-                                                  index == 15 ||
-                                                  index == 57 ||
-                                                  index == 59 ||
-                                                  index == 37 ||
-                                                  index == 43 ||
-                                                  index == 95 ||
-                                                  index ==
-                                                      93 || //=> posizioni[]
-                                                  index ==
-                                                      79 || //=> posizioni[8][7]
-                                                  index ==
-                                                      73 || //=> posizioni[8][1]
-                                                  index ==
-                                                      112) //=> posizioni[12][4]
-                                              ? Draggable(
-                                                  child: Icon(
-                                                    Soccerplayer.soccer_player,
-                                                    size: widget.lato,
-                                                  ),
-                                                  feedback: Icon(
-                                                    Soccerplayer.soccer_player,
-                                                    size: widget.lato,
-                                                  ),
-                                                  childWhenDragging: Icon(
-                                                    Soccerplayer.soccer_player,
-                                                    size: widget.lato,
-                                                  ),
-                                                )
-                                              : Container(
-                                                  decoration: BoxDecoration(
-                                                  color: Colors.transparent,
-                                                  shape: BoxShape.rectangle,
-                                                ));
-                                        })
-                                      : Container(
-                                          decoration: BoxDecoration(
-                                          color: Colors.transparent,
-                                          shape: BoxShape.rectangle,
-                                        ))
+                          ? state is FootballFieldCreated
+                              ? _getPlayerOrPlaceHolder(index)
+                              : state is FootballFieldEdit
+                                  ? DragTarget(builder: (BuildContext context,
+                                      List candidateData, List rejectedData) {
+                                      return (index == 11 ||
+                                              index == 15 ||
+                                              index == 57 ||
+                                              index == 59 ||
+                                              index == 37 ||
+                                              index == 43 ||
+                                              index == 95 ||
+                                              index == 93 || //=> posizioni[]
+                                              index ==
+                                                  79 || //=> posizioni[8][7]
+                                              index ==
+                                                  73 || //=> posizioni[8][1]
+                                              index ==
+                                                  112) //=> posizioni[12][4]
+                                          ? Draggable(
+                                              child: Icon(
+                                                Soccerplayer.soccer_player,
+                                                size: widget.lato,
+                                              ),
+                                              feedback: Icon(
+                                                Soccerplayer.soccer_player,
+                                                size: widget.lato,
+                                              ),
+                                              childWhenDragging: Icon(
+                                                Soccerplayer.soccer_player,
+                                                size: widget.lato,
+                                              ),
+                                            )
+                                          : Container(
+                                              decoration: BoxDecoration(
+                                              color: Colors.transparent,
+                                              shape: BoxShape.rectangle,
+                                            ));
+                                    })
+                                  : Container(
+                                      decoration: BoxDecoration(
+                                      color: Colors.transparent,
+                                      shape: BoxShape.rectangle,
+                                    ))
                           : Container(
                               decoration: BoxDecoration(
                               color: Colors.transparent,
                               shape: BoxShape.rectangle,
                             )))));
         });
+  }
+
+// Controllo se nella mappa dei giocatori già inseriti è presente qualcuno per questo indice
+// se si lo ritorno altrimenti restituisco un placeholder
+  Widget _getPlayerOrPlaceHolder(int index) {
+    Map<int, Player> players = widget.playersPlaced;
+    if (players.containsKey(index)) {
+      // Ritorno i dati del giocatore
+      return GestureDetector(
+          // This does not give the tap position ...
+          onLongPress: _showCustomMenu,
+          onTapDown: _storePosition,
+          onTap: () => {_tapIndex = index},
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Soccerplayer.soccer_player,
+                  size: widget.lato,
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(players[index].numero),
+                    Text(players[index].name)
+                  ],
+                )
+              ],
+            ),
+          ));
+    } else {
+      // Ritorno un placeholder
+      return GestureDetector(
+          onLongPress: _showCustomMenu,
+          onTapDown: _storePosition,
+          child: Container(
+            child: Icon(
+              Soccerplayer.soccer_player,
+              size: widget.lato,
+            ),
+          ));
+    }
   }
 }
 
@@ -145,6 +172,15 @@ class PlayerMenu extends PopupMenuEntry<int> {
   double height = 100;
   // height doesn't matter, as long as we are not giving
   // initialValue to showMenu().
+
+//Questo valore mi dice se ho fatto longPress su una cella con un giocatore
+// o con un placeholder
+  final Map<int, Player> _playerPlaced;
+  final List<Player> players;
+  final int _tapIndex;
+
+  PlayerMenu(this._playerPlaced, this.players, this._tapIndex);
+
 
   @override
   bool represents(int n) => n == 1 || n == -1;
@@ -161,33 +197,62 @@ class PlayerMenuState extends State<PlayerMenu> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        FlatButton(
-            onPressed: _plus1,
-            child: Center(
-                child: Row(
-              children: <Widget>[
-                Text('Add note'),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
-                ),
-                Icon(Icons.note_add)
-              ],
-            ))),
-        FlatButton(
-            onPressed: _plus1,
-            child: Center(
-                child: Row(
-              children: <Widget>[
-                Text('Substitution'),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
-                ),
-                Icon(Icons.shuffle)
-              ],
-            )))
-      ],
-    );
+    return widget._playerPlaced.containsKey(widget._tapIndex)
+        ? Column(
+            children: <Widget>[
+              FlatButton(
+                  onPressed: _plus1,
+                  child: Center(
+                      child: Row(
+                    children: <Widget>[
+                      Text('Add note'),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
+                      ),
+                      Icon(Icons.note_add)
+                    ],
+                  ))),
+              FlatButton(
+                  onPressed: _plus1,
+                  child: Center(
+                      child: Row(
+                    children: <Widget>[
+                      Text('Substitution'),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
+                      ),
+                      Icon(Icons.shuffle)
+                    ],
+                  ))),
+              FlatButton(
+                  onPressed: _plus1,
+                  child: Center(
+                      child: Row(
+                    children: <Widget>[
+                      Text('Remove'),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
+                      ),
+                      Icon(Icons.delete)
+                    ],
+                  )))
+            ],
+          )
+        : ListView.builder(
+            itemCount: widget.players.length,
+            itemBuilder: (context, index) {
+              return FlatButton(
+                  onPressed: _plus1,
+                  child: Center(
+                      child: Row(
+                    children: <Widget>[
+                      Text(widget.players[index].name),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
+                      ),
+                      Text(widget.players[index].numero),
+                    ],
+                  )));
+            });
   }
 }
